f5d7786e7096b7bcb4bfb7d1f9d72a787fb450c4
Maksym Khomiak <khomiakmaxim@gmail.com>
2024-11-04 12:07:46 +0000

    Init commit

---
+++ tests/solana_escrow.test.js
@@ -0,0 +1,13 @@
+const anchor = require("@coral-xyz/anchor");
+
+describe("solana_escrow", () => {
+  // Configure the client to use the local cluster.
+  anchor.setProvider(anchor.AnchorProvider.env());
+
+  it("Is initialized!", async () => {
+    // Add your test here.
+    const program = anchor.workspace.SolanaEscrow;
+    const tx = await program.methods.initialize().rpc();
+    console.log("Your transaction signature", tx);
+  });
+});
